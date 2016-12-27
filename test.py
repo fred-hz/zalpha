bb98c85ba1f54d2b74e11d0eae435954d4061682
fred-hz <fred.huangzou@gmail.com>
2016-12-27 18:20:54 +0800

    change config.xml

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
-fp = open(r'./data/listing_date.csv')
-content = fp.read().splitlines()
-fp.close()
+# fp = open(r'./data/listing_date.csv')
+# content = fp.read().splitlines()
+# fp.close()
+#
+# for line in content[1:]:
+#     items = line.split(',')
+#     if items[2] == '1':
+#         date = items[1][1:5]+items[1][6:8]+items[1][9:11]
+#         print(date)
+#
+#
 
-for line in content[1:]:
-    items = line.split(',')
-    if items[2] == '1':
-        date = items[1][1:5]+items[1][6:8]+items[1][9:11]
-        print(date)
+def func(a):
+    return a['c']
 
+a = {'a': 'a', 'b': 'b', 'c': {'a':'a', 'b':'b'}}
+print(id(a))
+print(id(a['c']))
+print(id(a['c']['a']))
 
+d = func(a)
+print(id(d))
+print(id(d['a']))
