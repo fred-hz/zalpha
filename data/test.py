88190d9c3694bb91ba4e1d84c1d1c6be8549f97f
TedZhao1993 <zibo.zhao1993@outlook.com>
2016-12-29 13:35:25 +0800

    few modification

--- data/test.py
+++ data/test.py
@@ -1,10 +1,18 @@
 import os, re
-data_path = 'F:\zalpha\zalpha\data'
-di_list = []
-self.ii_list = []
+address = r'F:\zalpha\zalpha\data\raw_stock_daily_data\20060104.csv'
+try:
+    with open(address) as fp:
+        content = fp.read().splitlines()
+except IOError:
+    print("file is missing!")
+else:
+    print("do next")
 
+'''
 address = data_path + '\\raw_stock_daily_data'
 files = os.listdir(address)
+output = open('STname.csv','w')
+name_list = []
 
 for file_ in files:
     print(file_)
@@ -12,8 +20,9 @@ for file_ in files:
         content = fp.read().splitlines()
     for line in content[1:]:
         items = line.replace('"', '').split(',')
-        items[1] = re.sub('\D','',items[1])
-        if items[1] not in self.ii_list:
-            self.ii_list.append(items[1])
-
-print(self.ii_list)
+        if 's' in items[2] or 'S' in items[2]:
+            if items[2] not in name_list:
+                name_list.append(items[2])
+                output.write(('%s' % items[2]) + '\n')
+output.close()
+'''
